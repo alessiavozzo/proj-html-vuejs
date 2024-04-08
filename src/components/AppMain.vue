a793a0d292ea9a63d63567d970685693e7745fb5
alessiavozzo <a.vozzo95@gmail.com>
2024-04-08 18:07:46 +0200

    add section subjects + static subject card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,8 +5,31 @@ export default {
 </script>
 
 <template>
-    main
+    <main id="site_main">
+
+        <section id="subjects">
+            <div class="container">
+                <div class="row">
+                    <div class="card d-flex col-2">
+                        <img src="/img/h5-custom-icon-1.png" alt="subject-icon">
+                        <h3>Languages</h3>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+.card {
+    border: 1px solid black;
+    padding: 1rem;
+    align-items: center;
+    flex-direction: column;
+    gap: 1.5rem;
+    background-color: var(--academy-subject);
+}
+</style>
