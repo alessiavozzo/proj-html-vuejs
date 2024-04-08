0eb2435445be2d1ebb4f2d87291b0c74718fc48c
alessiavozzo <a.vozzo95@gmail.com>
2024-04-08 18:17:18 +0200

    add dinamic subject cards

---
+++ src/assets/js/subjects.js
@@ -0,0 +1,26 @@
+export const subjects = [
+    {
+        image: "1.png",
+        name: "Languages"
+    },
+    {
+        image: "2.png",
+        name: "Software"
+    },
+    {
+        image: "3.png",
+        name: "Business"
+    },
+    {
+        image: "4.png",
+        name: "Chemistry"
+    },
+    {
+        image: "5.png",
+        name: "Science"
+    },
+    {
+        image: "6.png",
+        name: "DIY&Craft"
+    },
+]
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,13 @@
 <script>
+import { subjects } from '../assets/js/subjects';
 export default {
-    name: "AppMain"
+    name: "AppMain",
+    data() {
+        return {
+            subjects: subjects,
+            imageUrl: "/img/h5-custom-icon-"
+        }
+    }
 }
 </script>
 
@@ -10,9 +17,9 @@ export default {
         <section id="subjects">
             <div class="container">
                 <div class="row">
-                    <div class="card d-flex col-2">
-                        <img src="/img/h5-custom-icon-1.png" alt="subject-icon">
-                        <h3>Languages</h3>
+                    <div class="card d-flex col-2" v-for="subject in subjects">
+                        <img :src="`${imageUrl}${subject.image}`" alt="subject-icon">
+                        <h3>{{ subject.name }}</h3>
                     </div>
 
                 </div>
@@ -24,12 +31,16 @@ export default {
 
 
 <style scoped>
-.card {
-    border: 1px solid black;
-    padding: 1rem;
-    align-items: center;
-    flex-direction: column;
-    gap: 1.5rem;
-    background-color: var(--academy-subject);
+#subjects {
+    padding: 3rem 0;
+
+    .card {
+        border: 1px solid var(--subject-border);
+        padding: 2rem 1rem;
+        align-items: center;
+        flex-direction: column;
+        gap: 1.5rem;
+        background-color: var(--academy-subject);
+    }
 }
 </style>
