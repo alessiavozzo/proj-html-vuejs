9004be16688214e55d9a2ab5cd30fc70ff59b228
alessiavozzo <a.vozzo95@gmail.com>
2024-04-10 13:14:10 +0200

    add courses different datas

--- src/assets/js/courses.js
+++ src/assets/js/courses.js
@@ -27,25 +27,25 @@ export const courses = [
         type: "Business"
     },
     {
-        image: "course-1-f-img.jpg",
-        name: "Android Developer",
+        image: "course-4-f-img.jpg",
+        name: "Business English",
         price: "free",
-        author: "David Sanders",
+        author: "Doris Crawford",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus nemo eveniet nihil quod dicta impedit hic facere aspernatur inventore.",
         users: 1,
         type: "Programming"
     },
     {
-        image: "course-2-f-img.jpg",
-        name: "Web Designing",
-        price: "free",
+        image: "course-5-f-img.jpg",
+        name: "Leaders in Learning",
+        price: "$45",
         author: "Jennifer Powell",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus nemo eveniet nihil quod dicta impedit hic facere aspernatur inventore.",
         users: 1,
         type: "Programming"
     },
     {
-        image: "course-3-f-img.jpg",
+        image: "course-6-f-img.jpg",
         name: "Finantial Modeling",
         price: "$20",
         author: "Edward Bowman",
@@ -54,8 +54,8 @@ export const courses = [
         type: "Business"
     },
     {
-        image: "course-1-f-img.jpg",
-        name: "Android Developer",
+        image: "course-7-f-img.jpg",
+        name: "Basic Marketing",
         price: "free",
         author: "David Sanders",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus nemo eveniet nihil quod dicta impedit hic facere aspernatur inventore.",
@@ -63,16 +63,16 @@ export const courses = [
         type: "Programming"
     },
     {
-        image: "course-2-f-img.jpg",
-        name: "Web Designing",
-        price: "free",
+        image: "course-8-f-img.jpg",
+        name: "Python for Everybody",
+        price: "$25",
         author: "Jennifer Powell",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus nemo eveniet nihil quod dicta impedit hic facere aspernatur inventore.",
         users: 1,
         type: "Programming"
     },
     {
-        image: "course-3-f-img.jpg",
+        image: "course-9-f-img.jpg",
         name: "Finantial Modeling",
         price: "$20",
         author: "Edward Bowman",
